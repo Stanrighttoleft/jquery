3a8d4f7e413738f034b8198de16c5e77a9bef431
stan <moon_saga@yahoo.com.tw>
2025-03-04 12:01:46 +0800

    progress bar effect

--- js/app.js
+++ js/app.js
@@ -18,4 +18,37 @@ $(document).ready(function(){
         loopCount:Infinity,
         startDelay:1000
     });
+
+
+
+//porgress bars
+
+
+
+var p=document.querySelectorAll('.progress-bar');
+var sectionelm=document.querySelector('.about');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY >= sectionelm.offsetTop){
+        p[0].setAttribute("style","width:100%;background-color:#d6706c;transition:1s all");
+        p[1].setAttribute("style","width:95%;background-color:#d6706c;transition:1.5s all");
+        p[2].setAttribute("style","width:80%;background-color:#d6706c;transition:1.5s all");
+        p[3].setAttribute("style","width:20%;background-color:#d6706c;transition:1.5s all");
+        p[4].setAttribute("style","width:50%;background-color:#d6706c;transition:2s all");
+        p[5].setAttribute("style","width:60%;background-color:#d6706c;transition:2.5s all");
+
+
+    }
+    else{
+        p[0].setAttribute("style","width:0%;background-color:#d6706c;transition:4s all");
+        p[1].setAttribute("style","width:0%;background-color:#d6706c;transition:3s all");
+        p[2].setAttribute("style","width:0%;background-color:#d6706c;transition:1.5s all");
+        p[3].setAttribute("style","width:0%;background-color:#d6706c;transition:1.5s all");
+        p[4].setAttribute("style","width:0%;background-color:#d6706c;transition:2s all");
+        p[5].setAttribute("style","width:0%;background-color:#d6706c;transition:1s all");
+
+    }
 })
+
+})
+
+
